71aa07235bc6e375c1f1bacfccfe4a2f609b3fc0
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-07-30 13:40:07 +0700

    child-information to parent
    products-header -> home

---
+++ client/src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent implements OnInit {
+  cols: any | number = 3;
+  
+  constructor(
+
+  ) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+}
